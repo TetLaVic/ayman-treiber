2538c99179dde209e9b8f68792cc574d6149988e
Lavrenko <lavrenko@hs-ulm.de>
2021-08-05 17:35:14 +0200

    added minor changes

--- JS_Website.js
+++ JS_Website.js
@@ -1,3 +1,9 @@
+// References to the tags
+const standardViewRef = document.getElementById('standard');
+
+standardViewRef.addEventListener("click", standardView)
+
+
 // Man wird zurück zur Startseite geleitet, wenn man auf den "Zurück-Button" klickt
 function backFunction() {
     window.close();
@@ -5,12 +11,13 @@ function backFunction() {
 
 // Laden der Standard-Ansicht
 function standardView() {
+    let table = null;
     window.localStorage.setItem("view", "standard");
-    var standardViewColumns = ["treiber-name", "geraete", "akt-version", "leadDeveloper", "repositoryUrl", "labels", "compatible_hmi"];
-    var columnData = table.getColumnDefinitions();
-    var tick = [];
+    const standardViewColumns = ["treiber-name", "geraete", "akt-version", "leadDeveloper", "repositoryUrl", "labels", "compatible_hmi"];
+    const columnData = table.getColumnDefinitions();
+    let tick = [];
     //console.log(columnData.length);
-    for (var i = 0; i < columnData.length; i++) {
+    for (let i = 0; i < columnData.length; i++) {
         if (standardViewColumns.includes(columnData[i]["field"])) {
             table.showColumn(columnData[i]["field"]);
             tick[i] = true;
--- menu-items.js
+++ menu-items.js
@@ -7,7 +7,5 @@ function onitemclick(event) {
 if(event.target.nodeName!=="LI"){
 return;
 }
-// textRef.classList.remove("invisible");
-// textRef.classList.add("visible");
 textRef.classList.toggle("invisible");
 }
